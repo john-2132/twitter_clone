6eacb7408a6080b4e08566f99ee8820f7cf2e210
john-2132 <junichi.k0211@gmail.com>
2024-06-26 21:53:52 +0900

    githubログイン機能の実装 ・omniauth githubを導入し、githubアカウントでのログインを可能に ・ログアウトは通常のログイン時と同様に実施できる

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+         :confirmable, :lockable, :timeoutable, :trackable, :omniauthable, omniauth_providers: %i[github]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name
+    end
+  end
 
   VALID_PHONE_NUMBER_REGEX = /\A0(
     \d{1}[-(]?\d{4}|
@@ -14,5 +22,8 @@ class User < ApplicationRecord
     \d{4}[-(]?\d{1}
   )[-)]?\d{4}\z|\A0[5789]0-?\d{4}-?\d{4}\z/
 
-  validates :phone_number, presence: true, format: { with: VALID_PHONE_NUMBER_REGEX }
+  with_options unless: -> { validation_context == :omniauth } do
+    validates :phone_number, presence: true, format: { with: VALID_PHONE_NUMBER_REGEX }
+    validates :uid, uniqueness: { scope: :provider }
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_16_134850) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_23_223200) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,11 +42,14 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_16_134850) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "birth_date", null: false
-    t.string "phone_number", null: false
-    t.string "name", null: false
+    t.date "birth_date", default: "1930-01-01", null: false
+    t.string "phone_number", default: "", null: false
+    t.string "name", default: "", null: false
+    t.string "provider", default: "", null: false
+    t.string "uid", default: "", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end

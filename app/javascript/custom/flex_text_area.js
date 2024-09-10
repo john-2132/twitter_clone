6f8a62367a9251c8d266bc466a540984d9326fe9
john-2132 <junichi.k0211@gmail.com>
2024-09-10 22:18:00 +0900

    イベントリスナーへ登録するイベントを誤っていたため修正

--- app/javascript/custom/flex_text_area.js
+++ app/javascript/custom/flex_text_area.js
@@ -6,7 +6,7 @@ function flexTextarea(el) {
 }
 
 
-'turbo:load turbo:render'.split(' ').forEach( (eName) => {
+'turbo:load turbo:frame-render'.split(' ').forEach( (eName) => {
   document.addEventListener(eName, () => {
     document.querySelectorAll('.flex_text_area').forEach(flexTextarea)
   })

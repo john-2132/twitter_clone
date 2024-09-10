d78b4c3002fa3941cb4dc14b49cc41b8105e32e9
john-2132 <junichi.k0211@gmail.com>
2024-09-10 22:10:41 +0900

    ツイートポスト失敗時のrender後、入力欄を伸長させるjavascriptが効いていなかったので修正

--- app/javascript/custom/flex_text_area.js
+++ app/javascript/custom/flex_text_area.js
@@ -2,11 +2,12 @@ function flexTextarea(el) {
   const dummy = el.querySelector('.flex_text_area_dummy')
   el.querySelector('.flex_text_area_input_area').addEventListener('input', e => {
     dummy.textContent = e.target.value + '\u200b'
-    console.log(dummy.textContent);
   })
 }
 
 
-document.addEventListener('turbo:load', () => {
-  document.querySelectorAll('.flex_text_area').forEach(flexTextarea)
+'turbo:load turbo:render'.split(' ').forEach( (eName) => {
+  document.addEventListener(eName, () => {
+    document.querySelectorAll('.flex_text_area').forEach(flexTextarea)
+  })
 })

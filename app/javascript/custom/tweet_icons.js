db5ea34d3b8dd09928e5ab734932ee6a7e3ca154
john-2132 <junichi.k0211@gmail.com>
2024-09-24 23:42:57 +0900

    ・ツイートポスト後、ツイート入力欄の伸長のjavascriptが効いていなかったため修正 ・ツイートポスト、返信ポスト後、新しく作成されたポストのアイコンリンクを押下するためのjavascriptが効いていなかったため修正

--- app/javascript/custom/tweet_icons.js
+++ app/javascript/custom/tweet_icons.js
@@ -1,10 +1,8 @@
-'turbo:load turbo:frame-load after-favorite-render'.split(' ').forEach( (eName) => {
+'turbo:load turbo:frame-load after-tweet-render after-reply-render after-favorite-render'.split(' ').forEach( (eName) => {
   document.addEventListener(eName, () => {
     // no-link クラスが付いたボタンのクリックイベントをリンクに伝播させない
     document.querySelectorAll('.no-link').forEach(function(button) {
-      console.log('load');
       button.addEventListener('click', function(event) {
-        console.log('no_link');
         event.stopPropagation();
       });
     });

7f11b6be2ba0ffc48e370a1fd8ba6adf81786612
john-2132 <junichi.k0211@gmail.com>
2024-09-25 22:18:15 +0900

    リツイートアイコン押下時にブロックのリンクを作動させないjavascriptがリツイート後に効いていなかったため修正

--- app/javascript/custom/tweet_icons.js
+++ app/javascript/custom/tweet_icons.js
@@ -1,4 +1,4 @@
-'turbo:load turbo:frame-load after-tweet-render after-reply-render after-favorite-render'.split(' ').forEach( (eName) => {
+'turbo:load turbo:frame-load after-tweet-render after-reply-render after-favorite-render after-retweet-render'.split(' ').forEach( (eName) => {
   document.addEventListener(eName, () => {
     // no-link クラスが付いたボタンのクリックイベントをリンクに伝播させない
     document.querySelectorAll('.no-link').forEach(function(button) {
